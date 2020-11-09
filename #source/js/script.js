d8023840b7d0d6da77668b0da218b10cb6090176
Danyil Dorofieiev <dominiryem16@gmail.com>
2020-11-09 23:33:37 +0200

    test

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,10 +1,4 @@
-// $(document).ready(function () {
-// 	$('.hi__burger').click(function (event) {
-// 		$('.hi__burger,.hi__menu').toggleClass('active');
-// 	});
-// });
 var d = document.getElementsByClassName('hi__burger');
-// var hml = document.getElementsByClassName('hi__menu-link');
 $(document).ready(function () {
 	$('.hi__burger').on('click', function (e) {
 		$(d).toggleClass('active');
@@ -13,8 +7,3 @@ $(document).ready(function () {
 		$(d).removeClass('active');
 	});
 });
-// $(document).ready(function () {
-// 	$('.hi__menu-link').on('click', function (e) {
-// 		$(hml).toggleClass('hi__menu-link_active');
-// 	});
-// });
